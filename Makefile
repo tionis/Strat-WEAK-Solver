291a3d43a08f7957518dcf2fc5cccc284a276af8
tionis <git@tionis.dev>
2024-10-30 16:49:12 +0100

    added markdown table output

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 help: ## Show this help message
 	@grep -E '^[a-zA-Z_./-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
-plan: out.json out.md ## Plan the AKs
+plan: out.md table.md ## Plan the AKs
 
 setup: .venv/deps ## setup runtime
 
@@ -24,6 +24,9 @@ out.json: input.json .venv/deps
 
 out.md: out.json .venv/deps deps/generate_output_md.py
 	.venv/bin/python deps/generate_output_md.py out.json out.md
+
+table.md: out.json .venv/deps deps/generate_output_md_table.py
+	.venv/bin/python deps/generate_output_md_table.py out.json table.md
 
 input.json: config.yaml .venv/deps deps/generate_input_json.py
 	.venv/bin/python deps/generate_input_json.py config.yaml input.json
---
+++ deps/generate_output_md_table.py
@@ -0,0 +1,67 @@
+import json
+import sys
+
+def timeslot_id_to_numer(timeslot_id):
+    # trim prefix "Slot"
+    return int(timeslot_id[4:])
+
+def generate_output_md_table(output_json, output_file):
+    with open(output_json, "r") as f:
+        output = json.load(f)
+    
+    output_md = ""
+
+    ak_id_to_name = {}
+    room_id_to_name = {}
+    participant_id_to_name = {}
+    timeslot_id_to_name = {}
+
+    for ak in output["input"]["aks"]:
+        ak_id_to_name[ak["id"]] = ak["info"]["name"]
+
+    for room in output["input"]["rooms"]:
+        room_id_to_name[room["id"]] = room["info"]["name"]
+
+    for participant in output["input"]["participants"]:
+        participant_id_to_name[participant["id"]] = participant["info"]["name"]
+
+    for timeslot in output["input"]["timeslots"]["blocks"][0]:
+        timeslot_id_to_name[timeslot["id"]] = timeslot["info"]["start"]
+
+    timeslots = {}
+    for ak in output["scheduled_aks"]:
+        for timeslot in ak["timeslot_ids"]:
+            if timeslot not in timeslots:
+                timeslots[timeslot] = []
+            this_ak = {"name": ak_id_to_name[ak["ak_id"]], "room": room_id_to_name[ak["room_id"]], "participants": [participant_id_to_name[participant] for participant in ak["participant_ids"]]}
+            timeslots[timeslot].append(this_ak)
+
+    sorted_timeslots = sorted(timeslots.keys(),key=timeslot_id_to_numer)
+
+    max_concurrent_aks = max([len(timeslots[timeslot]) for timeslot in timeslots])
+
+    output_md += "| Time |"
+    for i in range(max_concurrent_aks):
+        output_md += f" AK {i+1} (Room) |"
+    output_md += "\n"
+
+    output_md += "| --- |"
+    for i in range(max_concurrent_aks):
+        output_md += " --- |"
+    output_md += "\n"
+
+    for timeslot in sorted_timeslots:
+        output_md += f"| {timeslot_id_to_name[timeslot]} |"
+        for ak in timeslots[timeslot]:
+            output_md += f" {ak['name']} ({ak['room']}) |"
+        for i in range(max_concurrent_aks - len(timeslots[timeslot])):
+            output_md += " |"
+        output_md += "\n"
+
+    with open(output_file, "w") as f:
+        f.write(output_md)
+
+if __name__ == "__main__":
+    # argv[1] is the output json file
+    # argv[2] is the output md table file
+    generate_output_md_table(sys.argv[1], sys.argv[2])
