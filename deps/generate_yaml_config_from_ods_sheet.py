3ffe3f808f967ecda9657e4d5cd4b4405518fe8b
tionis <me@tionis.dev>
2024-12-12 17:51:06 +0100

    added people parser

--- deps/generate_yaml_config_from_ods_sheet.py
+++ deps/generate_yaml_config_from_ods_sheet.py
@@ -21,6 +21,7 @@ def generate_yaml_config(input_ods_sheet_file, input_yaml_config_file, output_ya
     sheet = odsparsator.ods_to_python(input_ods_sheet_file)
     tables = sheet["body"]
     AKtable = None
+    ParticipantsTable = None
     for table in tables:
         if table["name"] == "AKs":
             AKtable = table["table"]
@@ -73,6 +74,9 @@ def generate_yaml_config(input_ods_sheet_file, input_yaml_config_file, output_ya
         room_constraints = []
         if len(cleanedCols) > 14:
             room_constraints = cleanedCols[14].split(",")
+        time_constraints = []
+        if len(cleanedCols) > 15:
+            time_constraints = cleanedCols[15].split(",")
         aks.append({
             "name": name,
             "protokoll": protokoll,
@@ -80,7 +84,8 @@ def generate_yaml_config(input_ods_sheet_file, input_yaml_config_file, output_ya
             "duration": duration,
             "head": head_field,
             "participants": participants+heads,
-            "room_constraints": room_constraints
+            "room_constraints": room_constraints,
+            "time_constraints": time_constraints
         })
         for head in heads:
             if not head in people:
@@ -91,10 +96,44 @@ def generate_yaml_config(input_ods_sheet_file, input_yaml_config_file, output_ya
                 people[participant] = {"name": "participant", "aks": []}
             people[participant]["aks"].append({"name": name})
 
+    # go over all rows in the participants table skipping the first row
+    # for all rows where the third column doesn't contain a checkmark symbol "✔" add the time_constraint "nach_freitag_ankunft"
+    for raw_row in ParticipantsTable[1:]:
+        row = raw_row["row"]
+        cleanedCols = []
+        for col in row:
+            # some cols have extra styling info, for these we take the 'value' key
+            typ = type(col)
+            if typ.__name__ == "dict" and "value" in col:
+                cleanedCols.append(col["value"])
+            else:
+                cleanedCols.append(col)
+        if len(cleanedCols) == 0:
+            break
+        if cleanedCols[1] is None or cleanedCols[1] == "" or cleanedCols[1] == "null":
+            break
+        name = cleanedCols[1]
+        if name == "" or name == "null":
+            break
+        if cleanedCols[2] is None or cleanedCols[1] == "" or cleanedCols[1] == "null":
+            print(f"{name} comes after friday noon")
+            person = people.get(name, {"name": "participant", "aks": []})
+            if not 'time_constraints' in person:
+                person['time_constraints'] = []
+            person['time_constraints'].append("nach_freitag_ankunft")
+            people[name] = person
+        else:
+            person = people.get(name, {"name": "participant", "aks": []})
+            if not 'time_constraints' in person:
+                person['time_constraints'] = []
+            people[name] = person
 
     config["aks"] = aks
     # Convert people from dict to list with key in name field
-    config["participants"] = [{"name": key, "aks": value["aks"]} for key, value in people.items()]
+    config["participants"] = [{"name": key,
+                               "aks": value["aks"],
+                               "time_constraints": value["time_constraints"]}
+                               for key, value in people.items()]
 
 
     os.makedirs(os.path.dirname(output_yaml_file), exist_ok=True)
